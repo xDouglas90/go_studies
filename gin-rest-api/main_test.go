a31fc28a8e7b63da0f6fba1af28c76595159d73a
odouglas.dev <odouglas.dev@gmail.com>
2022-11-07 17:57:17 -0300

    :test_tube: test: Verify Status Code from GET students

--- gin-rest-api/main_test.go
+++ gin-rest-api/main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 	"github.com/xdouglas90/go_studies/gin-rest-api/controllers"
+	"github.com/xdouglas90/go_studies/gin-rest-api/database"
 )
 
 func RoutesTestSetup() *gin.Engine {
@@ -22,11 +25,13 @@ func performRequest(r http.Handler, method, path string) *httptest.ResponseRecor
 }
 
 func TestVerifySCGetStudents(t *testing.T) {
+	database.Connect()
 	r := RoutesTestSetup()
 	r.GET("/students", controllers.GetStudents)
 	w := performRequest(r, "GET", "/students")
+	assert.Equal(t, http.StatusOK, w.Code, "OK response is expected")
 
-	if w.Code != http.StatusOK {
-		t.Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
-	}
+	resMock := `[{"ID":1,"CreatedAt":"2022-11-07T17:44:00.439386-03:00","UpdatedAt":"2022-11-07T17:44:00.439386-03:00","DeletedAt":null,"name":"Júlia Maria","cpf":"01958459003","rg":"209897574"}]`
+	resBody, _ := ioutil.ReadAll(w.Body)
+	assert.Equal(t, resMock, string(resBody), "Response body mismatch")
 }
